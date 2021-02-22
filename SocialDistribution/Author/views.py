dd0bd29dd28fa7442ccc7e81f0231f876b5a8bb3
OranRohoman <54954506+OranRohoman@users.noreply.github.com>
2021-02-22 16:26:30 -0700

    basic setup

--- SocialDistribution/Author/views.py
+++ SocialDistribution/Author/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseBadRequest,HttpResponse
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
+@api_view(["GET"])
+def open_path(request):
+    """
+        handles paths courses/
+        adds a course and gets a list of courses
+    """
+    return HttpResponse("Hello word")
