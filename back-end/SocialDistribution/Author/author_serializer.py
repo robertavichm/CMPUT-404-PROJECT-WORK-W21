828293bc235acae369b20aa06476c3e4a3b706fc
Ny-Tran <tntran1@ualberta.ca>
2021-02-28 02:31:31 -0700

    Add serializers (Post and Friendship)

--- back-end/SocialDistribution/Author/author_serializer.py
+++ back-end/SocialDistribution/Author/author_serializer.py
@@ -6,3 +6,16 @@ class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Author
         fields = ["id", "displayName", "host", "url", "type"]
+    
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Post
+        fields = ["post_id", "author_id","title", "type", "description", 
+                  "source", "origin", "contentType", "content", "categories",
+                  "commentLink", "commentCount", "pageSize", "published",
+                  "visibility", "unlisted"]
+
+class FriendshipSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.FriendShip
+        fields = ["FriendshipId", "author_primary", "author_friend", "accepted"]
