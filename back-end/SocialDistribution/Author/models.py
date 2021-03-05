15c9e98b21319fb0b0c2a6b5e596622810a2987a
justin <tan4@ualberta.ca>
2021-03-04 17:28:55 -0700

    Update changes

--- back-end/SocialDistribution/Author/models.py
+++ back-end/SocialDistribution/Author/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import AbstractUser
-from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import User
+from django.utils import timezone
+from django.contrib.postgres.fields import ArrayField
 import uuid
 # Create your models here.
 
