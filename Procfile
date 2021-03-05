91fa5b93a4b118f017cbd651764109cb41935720
Oran-Rohoman <oran@ualberta.ca>
2021-03-05 02:52:47 -0500

    procfile?

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python back-end/SocialDistribution/manage.py runserver 0.0.0.0:$PORT
+web: gunicorn back-end/SocialDistribution/SocialDistribution.wsgi:application --log-file -
--- back-end/SocialDistribution/SocialDistribution/settings.py
+++ back-end/SocialDistribution/SocialDistribution/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from database_setup import *
 import os
 import dj_database_url
+import django_on_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
