067767d833b0fed0d65e17704b348920865aea4a
Oran-Rohoman <oran@ualberta.ca>
2021-03-28 23:07:03 -0400

    allow all

--- back-end/SocialDistribution/SocialDistribution/settings.py
+++ back-end/SocialDistribution/SocialDistribution/settings.py
@@ -34,7 +34,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost' ,'127.0.0.1','cmput-404-group17.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost' ,'127.0.0.1','cmput-404-group17.herokuapp.com','*']
 
 
 
